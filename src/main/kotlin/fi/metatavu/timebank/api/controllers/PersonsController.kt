5133f4b89b90c4b4788795419b11c89fca20041d
Iiro Anttila <iiro.a.anttila@gmail.com>
2024-06-06 14:47:46 +0300

    Changes according to requested changes

--- src/main/kotlin/fi/metatavu/timebank/api/controllers/PersonsController.kt
+++ src/main/kotlin/fi/metatavu/timebank/api/controllers/PersonsController.kt
@@ -3,6 +3,7 @@ package fi.metatavu.timebank.api.controllers
 import fi.metatavu.timebank.model.PersonTotalTime
 import fi.metatavu.timebank.api.forecast.ForecastService
 import fi.metatavu.timebank.api.forecast.models.ForecastPerson
+import fi.metatavu.timebank.api.impl.translate.PersonsTranslator
 import fi.metatavu.timebank.api.keycloak.KeycloakController
 import fi.metatavu.timebank.api.utils.VacationUtils
 import org.slf4j.Logger
@@ -36,6 +37,9 @@ class PersonsController {
     @Inject
     lateinit var keycloakController: KeycloakController
 
+    @Inject
+    lateinit var personsTranslator: PersonsTranslator
+
     /**
      * Updates Person minimumBillableRate in Keycloak
      *
@@ -52,26 +56,7 @@ class PersonsController {
 
         keycloakController.updateUsersMinimumBillableRate(keycloakUser, person.minimumBillableRate)
 
-        return Person(
-            id = person.id,
-            firstName = person.firstName,
-            lastName = person.firstName,
-            email = person.email,
-            monday = person.monday,
-            tuesday = person.tuesday,
-            wednesday = person.wednesday,
-            thursday = person.thursday,
-            friday = person.friday,
-            saturday = person.saturday,
-            sunday = person.sunday,
-            active = person.active,
-            unspentVacations = person.unspentVacations,
-            spentVacations = person.spentVacations,
-            minimumBillableRate = keycloakController.getUsersMinimumBillableRate(keycloakUser),
-            language = person.language,
-            startDate = person.startDate,
-            keycloakId = person.keycloakId
-        )
+        return personsTranslator.translate(findPerson(person.id))
     }
 
     /**
@@ -85,7 +70,7 @@ class PersonsController {
             forecastService.getPersons().filter { !it.isSystemUser }
         } catch (e: Error) {
             logger.error("Error when requesting persons from Forecast API: ${e.localizedMessage}")
-            throw Error(e.localizedMessage)
+            throw Error(e.cause)
         }
     }
 
@@ -125,12 +110,6 @@ class PersonsController {
     suspend fun listPersons(active: Boolean? = true): List<ForecastPerson>? {
         val persons = getPersonsFromForecast()
 
-        persons.forEach { forecastPerson ->
-            val vacationAmounts = vacationUtils.getPersonsVacations(forecastPerson)
-            forecastPerson.unspentVacations = vacationAmounts.first
-            forecastPerson.spentVacations = vacationAmounts.second
-        }
-
         return if (active == false) {
             persons
         } else {
@@ -138,11 +117,14 @@ class PersonsController {
         }
     }
 
-    suspend fun findPerson(personId: Int): ForecastPerson? {
-        val person = listPersons()?.find { it.id == personId }
+    /**
+     * Finds a Forecast person with id
+     *
+     * @param personId Int
+     * @return ForecastPerson
+     */
+    suspend fun findPerson(personId: Int): ForecastPerson = forecastService.findPerson(personId)
 
-        return person
-    }
 
     /**
      * Makes List of PersonTotalTimes
