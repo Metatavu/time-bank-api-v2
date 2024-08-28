d1cd82cdc6c1e7a7bf0d3fa3401df9138091c687
Iiro Anttila <iiro.a.anttila@gmail.com>
2024-08-28 09:50:47 +0300

    removed unused import.

--- src/main/kotlin/fi/metatavu/timebank/api/impl/UsersApi.kt
+++ src/main/kotlin/fi/metatavu/timebank/api/impl/UsersApi.kt
@@ -3,7 +3,6 @@ package fi.metatavu.timebank.api.impl
 import fi.metatavu.timebank.api.controllers.UsersController
 import fi.metatavu.timebank.api.impl.translate.UserTranslator
 import fi.metatavu.timebank.spec.UsersApi
-import org.slf4j.Logger
 import java.util.*
 import javax.enterprise.context.RequestScoped
 import javax.inject.Inject
